9f65605f5e47e12f72454291acf6337544415427
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2025-01-27 16:41:43 -0700

    remove test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_16_020538) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "published_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[7.2].define(version: 0) do
 end
